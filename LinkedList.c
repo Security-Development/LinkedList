1c627d86e770b791243828ebb9b5b6ba21aba49f
Security-Development <35417717+Security-Development@users.noreply.github.com>
2022-03-18 08:47:23 +0900

    Create LinkedList.c

---
+++ LinkedList.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Type {
+	int r;
+	struct Type *t;
+} Type;
+
+
+Type* push(Type * n, int r) {
+	Type *t = (Type *)malloc(sizeof(Type));
+	t->r = r;
+	t->t = n->t;
+	
+	n->t = t;
+	return t;	
+}
+
+
+int main() {
+	Type *t = (Type *)malloc(sizeof(Type));
+	t->t = NULL;
+	
+	push(t, 10);
+	push(t, 20);
+	push(t, 30);
+	push(t, 40);
+	
+	Type *r = t->t;
+	
+	while( t != NULL) {
+		
+		printf("%d\n", r->r);
+		r = r->t;
+	}
+	
+
+}
